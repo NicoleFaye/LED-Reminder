0ef2b9258c79d7f3d503fa4510b17df0f3b4ec0f
NicoleFaye <41876584+NicoleFaye@users.noreply.github.com>
2024-07-03 12:48:02 -0700

    remove unused

--- main/tusb_msc_main.c
+++ main/tusb_msc_main.c
@@ -1,16 +1,3 @@
-/*
- * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Unlicense OR CC0-1.0
- */
-
-/* DESCRIPTION:
- * This example contains code to make ESP32-S3 based device recognizable by USB-hosts as a USB Mass Storage Device.
- * It either allows the embedded application i.e. example to access the partition or Host PC accesses the partition over USB MSC.
- * They can't be allowed to access the partition at the same time.
- * For different scenarios and behaviour, Refer to README of this example.
- */
-
 #include <errno.h>
 #include <dirent.h>
 #include "esp_console.h"
@@ -20,29 +7,29 @@
 #include "tinyusb.h"
 #include "tusb_msc_storage.h"
 #include "settings.h"
-#ifdef CONFIG_EXAMPLE_STORAGE_MEDIA_SDMMCCARD
-#include "diskio_impl.h"
-#include "diskio_sdmmc.h"
-#endif
 
 static const char *TAG = "example_main";
+static bool msc_exposed = true;
+static bool repl_enabled = false;
 
 /* TinyUSB descriptors
-   ********************************************************************* */
-#define EPNUM_MSC       1
+ ********************************************************************* */
+#define EPNUM_MSC 1
 #define TUSB_DESC_TOTAL_LEN (TUD_CONFIG_DESC_LEN + TUD_MSC_DESC_LEN)
 
-enum {
+enum
+{
     ITF_NUM_MSC = 0,
     ITF_NUM_TOTAL
 };
 
-enum {
+enum
+{
     EDPT_CTRL_OUT = 0x00,
-    EDPT_CTRL_IN  = 0x80,
+    EDPT_CTRL_IN = 0x80,
 
-    EDPT_MSC_OUT  = 0x01,
-    EDPT_MSC_IN   = 0x81,
+    EDPT_MSC_OUT = 0x01,
+    EDPT_MSC_IN = 0x81,
 };
 
 static uint8_t const desc_configuration[] = {
@@ -67,15 +54,14 @@ static tusb_desc_device_t descriptor_config = {
     .iManufacturer = 0x01,
     .iProduct = 0x02,
     .iSerialNumber = 0x03,
-    .bNumConfigurations = 0x01
-};
+    .bNumConfigurations = 0x01};
 
 static char const *string_desc_arr[] = {
-    (const char[]) { 0x09, 0x04 },  // 0: is supported language is English (0x0409)
-    "TinyUSB",                      // 1: Manufacturer
-    "TinyUSB Device",               // 2: Product
-    "123456",                       // 3: Serials
-    "Example MSC",                  // 4. MSC
+    (const char[]){0x09, 0x04}, // 0: is supported language is English (0x0409)
+    "TinyUSB",                  // 1: Manufacturer
+    "TinyUSB Device",           // 2: Product
+    "123456",                   // 3: Serials
+    "Example MSC",              // 4. MSC
 };
 /*********************************************************************** TinyUSB descriptors*/
 
@@ -124,8 +110,7 @@ const esp_console_cmd_t cmds[] = {
         .help = "exit from application",
         .hint = NULL,
         .func = &console_exit,
-    }
-};
+    }};
 
 // mount the partition and show all the files in BASE_PATH
 static void _mount(void)
@@ -137,18 +122,23 @@ static void _mount(void)
     ESP_LOGI(TAG, "\nls command output:");
     struct dirent *d;
     DIR *dh = opendir(BASE_PATH);
-    if (!dh) {
-        if (errno == ENOENT) {
-            //If the directory is not found
+    if (!dh)
+    {
+        if (errno == ENOENT)
+        {
+            // If the directory is not found
             ESP_LOGE(TAG, "Directory doesn't exist %s", BASE_PATH);
-        } else {
-            //If the directory is not readable then throw error and exit
+        }
+        else
+        {
+            // If the directory is not readable then throw error and exit
             ESP_LOGE(TAG, "Unable to read directory %s", BASE_PATH);
         }
         return;
     }
-    //While the next entry is not readable we will print directory files
-    while ((d = readdir(dh)) != NULL) {
+    // While the next entry is not readable we will print directory files
+    while ((d = readdir(dh)) != NULL)
+    {
         printf("%s\n", d->d_name);
     }
     return;
@@ -157,7 +147,8 @@ static void _mount(void)
 // unmount storage
 static int console_unmount(int argc, char **argv)
 {
-    if (tinyusb_msc_storage_in_use_by_usb_host()) {
+    if (tinyusb_msc_storage_in_use_by_usb_host())
+    {
         ESP_LOGE(TAG, "storage is already exposed");
         return -1;
     }
@@ -169,19 +160,22 @@ static int console_unmount(int argc, char **argv)
 // read BASE_PATH/settings.txt and print its contents
 static int console_read(int argc, char **argv)
 {
-    if (tinyusb_msc_storage_in_use_by_usb_host()) {
+    if (tinyusb_msc_storage_in_use_by_usb_host())
+    {
         ESP_LOGE(TAG, "storage exposed over USB. Application can't read from storage.");
         return -1;
     }
     ESP_LOGD(TAG, "read from storage:");
     const char *filename = BASE_PATH "/settings.txt";
     FILE *ptr = fopen(filename, "r");
-    if (ptr == NULL) {
+    if (ptr == NULL)
+    {
         ESP_LOGE(TAG, "Filename not present - %s", filename);
         return -1;
     }
     char buf[1024];
-    while (fgets(buf, 1000, ptr) != NULL) {
+    while (fgets(buf, 1000, ptr) != NULL)
+    {
         printf("%s", buf);
     }
     fclose(ptr);
@@ -191,14 +185,16 @@ static int console_read(int argc, char **argv)
 // create file BASE_PATH/settings.txt if it does not exist
 static int console_write(int argc, char **argv)
 {
-    if (tinyusb_msc_storage_in_use_by_usb_host()) {
+    if (tinyusb_msc_storage_in_use_by_usb_host())
+    {
         ESP_LOGE(TAG, "storage exposed over USB. Application can't write to storage.");
         return -1;
     }
     ESP_LOGD(TAG, "write to storage:");
     const char *filename = BASE_PATH "/settings.txt";
     FILE *fd = fopen(filename, "r");
-    if (!fd) {
+    if (!fd)
+    {
         ESP_LOGW(TAG, "settings.txt doesn't exist yet, creating");
         fd = fopen(filename, "w");
         fprintf(fd, settings_txt);
@@ -210,13 +206,14 @@ static int console_write(int argc, char **argv)
 // Show storage size and sector size
 static int console_size(int argc, char **argv)
 {
-    if (tinyusb_msc_storage_in_use_by_usb_host()) {
+    if (tinyusb_msc_storage_in_use_by_usb_host())
+    {
         ESP_LOGE(TAG, "storage exposed over USB. Application can't access storage");
         return -1;
     }
     uint32_t sec_count = tinyusb_msc_storage_get_sector_count();
     uint32_t sec_size = tinyusb_msc_storage_get_sector_size();
-    printf("Storage Capacity %lluMB\n", ((uint64_t) sec_count) * sec_size / (1024 * 1024));
+    printf("Storage Capacity %lluMB\n", ((uint64_t)sec_count) * sec_size / (1024 * 1024));
     return 0;
 }
 
@@ -243,153 +240,74 @@ static void storage_mount_changed_cb(tinyusb_msc_event_t *event)
     ESP_LOGI(TAG, "Storage mounted to application: %s", event->mount_changed_data.is_mounted ? "Yes" : "No");
 }
 
-#ifdef CONFIG_EXAMPLE_STORAGE_MEDIA_SPIFLASH
 static esp_err_t storage_init_spiflash(wl_handle_t *wl_handle)
 {
     ESP_LOGI(TAG, "Initializing wear levelling");
 
     const esp_partition_t *data_partition = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_FAT, NULL);
-    if (data_partition == NULL) {
+    if (data_partition == NULL)
+    {
         ESP_LOGE(TAG, "Failed to find FATFS partition. Check the partition table.");
         return ESP_ERR_NOT_FOUND;
     }
 
     return wl_mount(data_partition, wl_handle);
 }
-#else  // CONFIG_EXAMPLE_STORAGE_MEDIA_SPIFLASH
-static esp_err_t storage_init_sdmmc(sdmmc_card_t **card)
-{
-    esp_err_t ret = ESP_OK;
-    bool host_init = false;
-    sdmmc_card_t *sd_card;
-
-    ESP_LOGI(TAG, "Initializing SDCard");
-
-    // By default, SD card frequency is initialized to SDMMC_FREQ_DEFAULT (20MHz)
-    // For setting a specific frequency, use host.max_freq_khz (range 400kHz - 40MHz for SDMMC)
-    // Example: for fixed frequency of 10MHz, use host.max_freq_khz = 10000;
-    sdmmc_host_t host = SDMMC_HOST_DEFAULT();
-
-    // This initializes the slot without card detect (CD) and write protect (WP) signals.
-    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
-    sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
-
-    // For SD Card, set bus width to use
-#ifdef CONFIG_EXAMPLE_SDMMC_BUS_WIDTH_4
-    slot_config.width = 4;
-#else
-    slot_config.width = 1;
-#endif  // CONFIG_EXAMPLE_SDMMC_BUS_WIDTH_4
-
-    // On chips where the GPIOs used for SD card can be configured, set the user defined values
-#ifdef CONFIG_SOC_SDMMC_USE_GPIO_MATRIX
-    slot_config.clk = CONFIG_EXAMPLE_PIN_CLK;
-    slot_config.cmd = CONFIG_EXAMPLE_PIN_CMD;
-    slot_config.d0 = CONFIG_EXAMPLE_PIN_D0;
-#ifdef CONFIG_EXAMPLE_SDMMC_BUS_WIDTH_4
-    slot_config.d1 = CONFIG_EXAMPLE_PIN_D1;
-    slot_config.d2 = CONFIG_EXAMPLE_PIN_D2;
-    slot_config.d3 = CONFIG_EXAMPLE_PIN_D3;
-#endif  // CONFIG_EXAMPLE_SDMMC_BUS_WIDTH_4
-
-#endif  // CONFIG_SOC_SDMMC_USE_GPIO_MATRIX
-
-    // Enable internal pullups on enabled pins. The internal pullups
-    // are insufficient however, please make sure 10k external pullups are
-    // connected on the bus. This is for debug / example purpose only.
-    slot_config.flags |= SDMMC_SLOT_FLAG_INTERNAL_PULLUP;
-
-    // not using ff_memalloc here, as allocation in internal RAM is preferred
-    sd_card = (sdmmc_card_t *)malloc(sizeof(sdmmc_card_t));
-    ESP_GOTO_ON_FALSE(sd_card, ESP_ERR_NO_MEM, clean, TAG, "could not allocate new sdmmc_card_t");
-
-    ESP_GOTO_ON_ERROR((*host.init)(), clean, TAG, "Host Config Init fail");
-    host_init = true;
-
-    ESP_GOTO_ON_ERROR(sdmmc_host_init_slot(host.slot, (const sdmmc_slot_config_t *) &slot_config),
-                      clean, TAG, "Host init slot fail");
-
-    while (sdmmc_card_init(&host, sd_card)) {
-        ESP_LOGE(TAG, "The detection pin of the slot is disconnected(Insert uSD card). Retrying...");
-        vTaskDelay(pdMS_TO_TICKS(3000));
-    }
-
-    // Card has been initialized, print its properties
-    sdmmc_card_print_info(stdout, sd_card);
-    *card = sd_card;
-
-    return ESP_OK;
-
-clean:
-    if (host_init) {
-        if (host.flags & SDMMC_HOST_FLAG_DEINIT_ARG) {
-            host.deinit_p(host.slot);
-        } else {
-            (*host.deinit)();
-        }
-    }
-    if (sd_card) {
-        free(sd_card);
-        sd_card = NULL;
-    }
-    return ret;
-}
-#endif  // CONFIG_EXAMPLE_STORAGE_MEDIA_SPIFLASH
 
 void app_main(void)
 {
     ESP_LOGI(TAG, "Initializing storage...");
 
-#ifdef CONFIG_EXAMPLE_STORAGE_MEDIA_SPIFLASH
+    // Initialize wear levelling
     static wl_handle_t wl_handle = WL_INVALID_HANDLE;
     ESP_ERROR_CHECK(storage_init_spiflash(&wl_handle));
 
+    // Initialize MSC storage
     const tinyusb_msc_spiflash_config_t config_spi = {
         .wl_handle = wl_handle,
-        .callback_mount_changed = storage_mount_changed_cb,  /* First way to register the callback. This is while initializing the storage. */
+        .callback_mount_changed = storage_mount_changed_cb, /* First way to register the callback. This is while initializing the storage. */
         .mount_config.max_files = 5,
     };
     ESP_ERROR_CHECK(tinyusb_msc_storage_init_spiflash(&config_spi));
     ESP_ERROR_CHECK(tinyusb_msc_register_callback(TINYUSB_MSC_EVENT_MOUNT_CHANGED, storage_mount_changed_cb)); /* Other way to register the callback i.e. registering using separate API. If the callback had been already registered, it will be overwritten. */
-#else // CONFIG_EXAMPLE_STORAGE_MEDIA_SPIFLASH
-    static sdmmc_card_t *card = NULL;
-    ESP_ERROR_CHECK(storage_init_sdmmc(&card));
 
-    const tinyusb_msc_sdmmc_config_t config_sdmmc = {
-        .card = card,
-        .callback_mount_changed = storage_mount_changed_cb,  /* First way to register the callback. This is while initializing the storage. */
-        .mount_config.max_files = 5,
-    };
-    ESP_ERROR_CHECK(tinyusb_msc_storage_init_sdmmc(&config_sdmmc));
-    ESP_ERROR_CHECK(tinyusb_msc_register_callback(TINYUSB_MSC_EVENT_MOUNT_CHANGED, storage_mount_changed_cb)); /* Other way to register the callback i.e. registering using separate API. If the callback had been already registered, it will be overwritten. */
-#endif  // CONFIG_EXAMPLE_STORAGE_MEDIA_SPIFLASH
-
-    //mounted in the app by default
+    // Mounted in the app by default
     _mount();
 
-    ESP_LOGI(TAG, "USB MSC initialization");
-    const tinyusb_config_t tusb_cfg = {
-        .device_descriptor = &descriptor_config,
-        .string_descriptor = string_desc_arr,
-        .string_descriptor_count = sizeof(string_desc_arr) / sizeof(string_desc_arr[0]),
-        .external_phy = false,
-        .configuration_descriptor = desc_configuration,
-    };
-    ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
-    ESP_LOGI(TAG, "USB MSC initialization DONE");
+    // create the settings.txt file if it does not exist
+    console_write(0, NULL);
 
-    esp_console_repl_t *repl = NULL;
-    esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
-    /* Prompt to be printed before each line.
-     * This can be customized, made dynamic, etc.
-     */
-    repl_config.prompt = PROMPT_STR ">";
-    repl_config.max_cmdline_length = 64;
-    esp_console_register_help_command();
-    esp_console_dev_uart_config_t hw_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_console_new_repl_uart(&hw_config, &repl_config, &repl));
-    for (int count = 0; count < sizeof(cmds) / sizeof(esp_console_cmd_t); count++) {
-        ESP_ERROR_CHECK(esp_console_cmd_register(&cmds[count]));
+    // Initialize USB MSC
+    if (msc_exposed)
+    {
+        ESP_LOGI(TAG, "USB MSC initialization");
+        const tinyusb_config_t tusb_cfg = {
+            .device_descriptor = &descriptor_config,
+            .string_descriptor = string_desc_arr,
+            .string_descriptor_count = sizeof(string_desc_arr) / sizeof(string_desc_arr[0]),
+            .external_phy = false,
+            .configuration_descriptor = desc_configuration,
+        };
+        ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
+        ESP_LOGI(TAG, "USB MSC initialization DONE");
     }
-    ESP_ERROR_CHECK(esp_console_start_repl(repl));
+
+    // Initialize REPL
+    if (repl_enabled)
+    {
+        esp_console_repl_t *repl = NULL;
+        esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
+        // Prompt to be printed before each line.
+        // This can be customized, made dynamic, etc.
+        repl_config.prompt = PROMPT_STR ">";
+        repl_config.max_cmdline_length = 64;
+        esp_console_register_help_command();
+        esp_console_dev_uart_config_t hw_config = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
+        ESP_ERROR_CHECK(esp_console_new_repl_uart(&hw_config, &repl_config, &repl));
+        for (int count = 0; count < sizeof(cmds) / sizeof(esp_console_cmd_t); count++)
+        {
+            ESP_ERROR_CHECK(esp_console_cmd_register(&cmds[count]));
+        }
+        ESP_ERROR_CHECK(esp_console_start_repl(repl));
+    }
 }
